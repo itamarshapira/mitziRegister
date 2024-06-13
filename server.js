c3462d489c9e95db80be80981d75f80f23ac9d58
itamarshapira <106903935+itamarshapira@users.noreply.github.com>
2024-06-13 13:09:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongojs = require("mongojs");
+//const { validateForm } = require('./static/register');
+//const { validateForm } = require('./static/validation');
   
 // the shared mongodb server:
 const db = mongojs(
@@ -38,9 +40,16 @@ app.post('/submit', (req, res) => {
       last_name: req.body.last_name,
       email: req.body.email,
       password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
       created_at: Date.now(),
       modified_at: Date.now(),
   };
+ 
+  if (!validateForm(newSubmit.first_name, newSubmit.last_name, newSubmit.email, newSubmit.password, newSubmit.confirmPassword)) {
+    // If validation fails, do not proceed
+    res.status(400).json({ error: 'SERVER SIDE: Register error' });
+    return;
+}
 
   tasks_coll.insert(newSubmit, (err, doc) => {
       if (err) {
@@ -50,6 +59,46 @@ app.post('/submit', (req, res) => {
       }
   });
 });
+
+// // DELETE request to delete a specific task
+// app.delete('/tasks/:id', (req, res) => {
+//   const taskId = mongojs.ObjectId(req.params.id);
+//   tasks_coll.remove({_id:taskId}, true,(err, doc) => {
+//     if (err) {
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     } else {
+//       res.json({msg:"Successfully deleted"}); 
+//     }
+//   });
+  
+// });
+
+
+
+const validateForm = (firstName, lastName, email, password, confirmPassword) => {
+  // Check if any field is empty
+  if (!firstName || !lastName || !email || !password || !confirmPassword) {
+      return false;   
+  }
+
+  // Check if password is at least 8 characters long
+  if (password.length < 8) {
+      return false;
+  }
+
+  // Check if password matches confirm password
+  if (password !== confirmPassword) {
+      return false;
+  }
+
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!re.test(email)) { // Using test() method
+      return false
+  }
+
+  // If all validations pass, return true
+  return true;
+}
 
 
 // Start the server
